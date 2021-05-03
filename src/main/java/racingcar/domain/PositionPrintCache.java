c4301850b4bc3e492d3492aacfd3c22e7b5e8fa4
jordy-torvalds <jordy-torvalds@kakao.com>
2021-05-04 05:04:38 +0900

    feat: 위치 출력 캐시 기능 및 테스트 구현

---
+++ src/main/java/racingcar/domain/PositionPrintCache.java
@@ -0,0 +1,61 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class PositionPrintCache {
+    private static Map<Integer, String> cache = new TreeMap<>();
+
+    public static int getSize() {
+        return cache.size();
+    }
+
+    public static String getPositionPrint(int key) {
+        boolean isContained = cache.containsKey(key);
+        String positionPrint = null;
+
+        if (isContained == true) {
+            positionPrint = cache.get(key);
+        } else if (isContained == false) {
+            cache.put(key, createNewCache(key));
+            positionPrint = cache.get(key);
+        }
+        return positionPrint;
+    }
+
+    public static void clearUnusedCache(int lowestValue) {
+        ArrayList<Integer> cacheKeyList = new ArrayList<>(cache.keySet());
+        int removingTargetIndex = lowestValue - GameMetaData.Position.DEFAULT_INCREASE_VALUE;
+
+        for (Integer cacheKey : cacheKeyList) {
+            removeMinValue(cacheKey, removingTargetIndex);
+        }
+    }
+
+    private static void removeMinValue(int key, int minValue) {
+        if (key <= minValue) {
+            cache.remove(key);
+        }
+    }
+
+    private static String createNewCache(int key) {
+        String newCache = null;
+        if (key == 0) {
+            newCache = new String("");
+        } else if (key >= GameMetaData.Position.DEFAULT_INCREASE_VALUE) {
+            newCache = createNewCacheString(key);
+        }
+        return newCache;
+    }
+
+    private static String createNewCacheString(int key) {
+        String result = cache.getOrDefault(key - GameMetaData.Position.DEFAULT_INCREASE_VALUE, "");
+
+        for (int i = 0; i < GameMetaData.Position.DEFAULT_INCREASE_VALUE; i++) {
+            result += "=";
+        }
+
+        return new String(result);
+    }
+}
---
+++ src/test/java/racingcar/domain/PositionPrintCacheTest.java
@@ -0,0 +1,62 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("위치_출력_캐시")
+class PositionPrintCacheTest {
+
+    @Test
+    void 위치_출력_정상_0부터_5까지() {
+        // Given
+        List<Integer> ints = Arrays.asList(0, 1, 2, 3, 4, 5);
+        List<String> expectedResults = Arrays.asList("", "=", "==", "===", "====", "=====");
+
+
+        for (int i = 0; i < ints.size(); i++) {
+            // When
+            String result = PositionPrintCache.getPositionPrint(ints.get(i));
+
+            // Then
+            assertEquals(expectedResults.get(i), result);
+        }
+    }
+
+    @Test
+    void 위치_출력_정상_동일한_위치_비중복_저장_확인() {
+        // Given
+        List<Integer> ints = Arrays.asList(0, 1, 1, 1, 1, 1);
+        int expectedResult = 2;
+
+        // When
+        for (int i = 0; i < ints.size(); i++) {
+            PositionPrintCache.getPositionPrint(ints.get(i));
+        }
+
+        // Then
+        assertEquals(expectedResult, PositionPrintCache.getSize());
+    }
+
+    @Test
+    void 위치_출력_정상_미사용_캐시_정리_확인() {
+        // Given
+        List<Integer> ints = Arrays.asList(0, 1, 2, 3, 4, 5);
+        int removingIndex = 3;
+        int expectedResult = 3;
+
+        // When
+        for (int i = 0; i < ints.size(); i++) {
+            PositionPrintCache.getPositionPrint(ints.get(i));
+        }
+        PositionPrintCache.clearUnusedCache(removingIndex);
+
+        // Then
+        assertEquals(expectedResult, PositionPrintCache.getSize());
+    }
+}
