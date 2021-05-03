6839decdf8db2ae21c21dd7025e9045e0461b52e
jordy-torvalds <jordy-torvalds@kakao.com>
2021-05-04 07:19:37 +0900

    feat: 재시도 횟수 입력 받기 기능 구현

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -3,8 +3,10 @@ package racingcar.view;
 import racingcar.domain.Position;
 import racingcar.domain.PositionPrintCache;
 import racingcar.domain.RacingCars;
+import racingcar.domain.TryCount;
 import racingcar.util.RacingCarException;
 import racingcar.util.RacingCarsException;
+import racingcar.util.TryCountException;
 
 import java.util.Optional;
 import java.util.Scanner;
@@ -14,11 +16,21 @@ import static java.lang.String.format;
 public class RacingCarView {
 
     private final String MESSAGE_ANSWER_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String MESSAGE_ANSWER_TRY_COUNT = "시도할 회수는 몇회인가요?";
     private final Scanner sc = new Scanner(System.in);
 
     public void run() {
-        initializeRacingCars();
-//        initializeTryCount();
+        RacingCars racingCars = initializeRacingCars();
+        TryCount tryCount = initializeTryCount();
+    }
+
+    public void runTry(RacingCars racingCars) {
+        racingCars.applyForwardStop();
+        racingCars.runRacingCarConsumer((racingCar) -> {
+            Position position = racingCar.getPosition();
+            String positionPrint = PositionPrintCache.getPositionPrint(position.getValue());
+            System.out.println(format("%s:%s", racingCar.getName(), positionPrint));
+        });
     }
 
     private RacingCars initializeRacingCars() {
@@ -45,12 +57,27 @@ public class RacingCarView {
         return optionalRacingCars;
     }
 
-    public void runTry(RacingCars racingCars) {
-        racingCars.applyForwardStop();
-        racingCars.runRacingCarConsumer((racingCar) -> {
-            Position position = racingCar.getPosition();
-            String positionPrint = PositionPrintCache.getPositionPrint(position.getValue());
-            System.out.println(format("%s:%s", racingCar.getName(), positionPrint));
-        });
+    private TryCount initializeTryCount() {
+        boolean isContinued = true;
+        Optional<TryCount> optionalTryCount = null;
+        System.out.println(MESSAGE_ANSWER_TRY_COUNT);
+        while (isContinued) {
+            optionalTryCount = createTryCount();
+            isContinued = !optionalTryCount.isPresent();
+        }
+        return optionalTryCount.get();
+    }
+
+    private Optional<TryCount> createTryCount() {
+        Optional<TryCount> optionalTryCount = null;
+        try {
+            String enteredTryCount = sc.nextLine();
+            TryCount tryCount = new TryCount(enteredTryCount);
+            optionalTryCount = Optional.of(tryCount);
+        } catch (TryCountException e) {
+            System.out.println(e.getMessage());
+            optionalTryCount = Optional.ofNullable(null);
+        }
+        return optionalTryCount;
     }
 }
