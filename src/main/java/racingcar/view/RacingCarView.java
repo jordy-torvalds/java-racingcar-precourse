daff0a9b6c2e6e8cbcba7908c51fb1e5ae92f25b
jordy-torvalds <jordy-torvalds@kakao.com>
2021-05-04 07:13:17 +0900

    feat: 게임 시작 및 자동차 이름 입력 받기 기능 구현

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -3,10 +3,47 @@ package racingcar.view;
 import racingcar.domain.Position;
 import racingcar.domain.PositionPrintCache;
 import racingcar.domain.RacingCars;
+import racingcar.util.RacingCarException;
+import racingcar.util.RacingCarsException;
+
+import java.util.Optional;
+import java.util.Scanner;
 
 import static java.lang.String.format;
 
 public class RacingCarView {
+
+    private final String MESSAGE_ANSWER_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final Scanner sc = new Scanner(System.in);
+
+    public void run() {
+        initializeRacingCars();
+//        initializeTryCount();
+    }
+
+    private RacingCars initializeRacingCars() {
+        boolean isContinued = true;
+        Optional<RacingCars> optionalRacingCars = null;
+        System.out.println(MESSAGE_ANSWER_CAR_NAME);
+        while (isContinued) {
+            optionalRacingCars = createRacingCar();
+            isContinued = !optionalRacingCars.isPresent();
+        }
+        return optionalRacingCars.get();
+    }
+
+    private Optional<RacingCars> createRacingCar() {
+        Optional<RacingCars> optionalRacingCars = null;
+        try {
+            String enteredCarName = sc.nextLine();
+            RacingCars racingCars = new RacingCars(enteredCarName);
+            optionalRacingCars = Optional.of(racingCars);
+        } catch (RacingCarsException | RacingCarException e) {
+            System.out.println(e.getMessage());
+            optionalRacingCars = Optional.ofNullable(null);
+        }
+        return optionalRacingCars;
+    }
 
     public void runTry(RacingCars racingCars) {
         racingCars.applyForwardStop();
