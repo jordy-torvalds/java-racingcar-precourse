ffce19ab84a04f698f3b5f061139d0b9331fb17f
jordy-torvalds <jordy-torvalds@kakao.com>
2021-05-04 07:12:46 +0900

    fix: 경주 자동차 예외 메시지 변경

--- src/main/java/racingcar/util/RacingCarException.java
+++ src/main/java/racingcar/util/RacingCarException.java
@@ -1,9 +1,8 @@
 package racingcar.util;
 
 import static java.lang.String.format;
-import static java.util.prefs.Preferences.MAX_NAME_LENGTH;
+import static racingcar.domain.GameMetaData.RacingCar.MAX_NAME_LENGTH;
 import static racingcar.domain.GameMetaData.RacingCar.MIN_NAME_LENGTH;
-import static racingcar.domain.GameMetaData.RacingCars.MIN_PLAYING_RACING_CARS_SIZE;
 
 public class RacingCarException extends RuntimeException {
     private RacingCarException(String message) {
@@ -15,7 +14,6 @@ public class RacingCarException extends RuntimeException {
             super(format("경주할 자동차 이름을 %d자 이상, %d자 이하로 입력하세요 / 입력하신 이름: %s",
                     MIN_NAME_LENGTH,
                     MAX_NAME_LENGTH,
-                    MIN_PLAYING_RACING_CARS_SIZE,
                     playerInput));
         }
     }
