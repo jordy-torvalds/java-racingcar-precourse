2d5d1af0077a5605e651250bb4348b430fd6aecd
jordy-torvalds <jordy-torvalds@kakao.com>
2021-05-04 22:31:02 +0900

    test: 재시도 횟수 차감 및 횟수 잔여 여부 확인 테스트 코드 추가, 부적절한 테스트 이름 변경

--- src/test/java/racingcar/domain/TryCountTest.java
+++ src/test/java/racingcar/domain/TryCountTest.java
@@ -1,15 +1,33 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.util.TryCountException;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("재시도_횟수")
 class TryCountTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,false", "2,true"})
+    @DisplayName("재시도_횟수_잔여_확인_기능_테스트")
+    void 재시도_횟수_잔여_확인_기능_테스트(String arg) {
+        // Given
+        String[] args = arg.split(",");
+        String tryCountArg = args[0];
+        String resultString = args[1];
+        boolean expectedResult = Boolean.parseBoolean(resultString);
+
+        // When
+        TryCount tryCount = new TryCount(tryCountArg);
+        tryCount.minusTryCount();
+
+        // Then
+        assertEquals(expectedResult, tryCount.isRemainingTryCount());
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"1", "3", "5", "7"})
@@ -29,9 +47,8 @@ class TryCountTest {
     @ParameterizedTest
     @ValueSource(strings = {"0", "-3", "-5", "-10"})
     @DisplayName("재시도_횟수_예외/0_미만_정수_입력")
-    void 재시도_횟수_예외_0_미만_정수(String args) {
+    void 재시도_횟수_예외_0_미만_정수_입력(String args) {
         assertThrows(TryCountException.IllegalMinCountException.class
                 , () -> new TryCount(args));
     }
-
 }
